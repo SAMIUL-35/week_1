abf060bc261e473cff4083d0ec0e7231c3546f8f
SAMIUL2022 <sami35bd@gmail.com>
2024-03-16 16:36:26 +0600

    merging array

---
+++ Day_2/A_Merging_Arrays.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int i = 0;i < n;i++) {
+        cin >> a[i];
+    }
+    for (int i = 0;i < m;i++) {
+        cin >> b[i];
+    }
+vector<int> c;
+    int l = 0, r = 0, ans = 0;
+
+    while (r < m || l < n) {
+        if (b[r] > a[l]  ) {
+            c.push_back(a[l]);
+            l++;
+              }
+       
+       else {
+          c.push_back(b[r]);
+          r++;
+       }
+       
+    }
+    for (int i = 0; i <c.size(); i++)
+    {
+        cout<<c[i]<<" ";
+    }
+    
+    return 0;
+}
