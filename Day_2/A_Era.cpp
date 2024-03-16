e49c18e68d4e13e7b487e01403d07195a02ac24d
SAMIUL2022 <sami35bd@gmail.com>
2024-03-16 11:05:34 +0600

    A.Era another version

--- Day_2/A_Era.cpp
+++ Day_2/A_Era.cpp
@@ -1,20 +1,39 @@
 //https://codeforces.com/problemset/problem/1604/A
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main () {
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int n;
+//         cin >> n;
+       
+//         int ans = 0;
+//         for (int i = 1; i <= n; i++) {
+//             int v;
+//             cin >> v; 
+//             ans=max(ans,v-i);
+//         }
+//         cout<<ans<<endl;
+//     }
+//     return 0;
+// }
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main () {
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
-       
+        vector<int> v(n+1);
         int ans = 0;
         for (int i = 1; i <= n; i++) {
-            int v;
-            cin >> v; 
-            ans=max(ans,v-i);
+            cin >> v[i]; 
+            ans=max(ans,v[i]-i);
         }
         cout<<ans<<endl;
     }
